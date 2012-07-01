bfbd528ab304897c230592d2f9473fcd9463e120
bmartin1031 <bmartin1031@gmail.com>
2012-07-01 18:22:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NightFly::Application.config.secret_token = '6a948f78c7547382777f6a5376699cf7a15f2ff0affb341651537f800ef537f49647648ed35bb3e090bbfb3dc9df15206fdab26a59e894038105958a8b214193'
